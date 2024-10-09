8cbd8b1ec1fd6665efe085c86b50ec3f1d218bbe
kkevxuu <kkevxu@gmail.com>
2024-10-09 13:04:28 -0400

    hw5 v1

--- solution-hw5/js/bakeshopcart.js
+++ solution-hw5/js/bakeshopcart.js
@@ -1,9 +1,9 @@
 //re-defining adjustment objects here because the ones defined in bakeshopcalc won't link up
 let glazeOptions = {
-    "Keep original": 0,
-    "Sugar milk": 0,
-    "Vanilla milk": 0.5,
-    "Double chocolate": 1.5,
+    "Keep Original": 0,
+    "Sugar Milk": 0,
+    "Vanilla Milk": 0.5,
+    "Double Chocolate": 1.5,
 };
 
 let packOptions = {
@@ -13,10 +13,8 @@ let packOptions = {
     12: 10,
 };
 
-
 //create cart array
-
-const cart = [];
+const cart = new Set();
 
 //create a roll constructor
 
@@ -39,7 +37,7 @@ let defaultRolls = [
 
 //function that takes roll objects and adds to cart
 for (const newRoll of defaultRolls){
-    cart.push(newRoll);
+    cart.add(newRoll);
 }
 
 console.log(cart);
@@ -50,6 +48,8 @@ for (const rollItem of cart ) {
     createCartObject(rollItem);
 }
 
+
+
 function createCartObject(rollItem) {
     console.log("Creating cart templates...");
     //create a template of a cart item in html
@@ -59,7 +59,7 @@ function createCartObject(rollItem) {
     console.log(rollItem.element);
 
     //adding rollItem.element to template list, the container of all cloned templates
-    const containerElement = document.querySelector("#cartContainer");
+const containerElement = document.querySelector("#cartContainer");
     containerElement.append(rollItem.element);
   
     //update element with input of object rollItem as it provides information for what needs to be updated
@@ -71,6 +71,8 @@ function createCartObject(rollItem) {
         deleteItem(rollItem);
     });
 }
+
+
 
 function updateElement(rollItem){
     //assigning values from rollItem to template
@@ -85,22 +87,30 @@ function updateElement(rollItem){
     templateName.innerText = `${rollItem.type} cinnamon roll`;
     templateGlaze.innerText = rollItem.glazing;
     templatePack.innerText = `Pack Size: ${rollItem.size}`;
-    templatePrice.innertext = rollItem.basePrice;
-   //price calculations
+    //displaying the item configurated price, use glazeOptions and packOptions objects defined in HW4 to access adjusted values
     const rollBasePrice = rollItem.basePrice;
-    console.log(rollBasePrice);
-    //use glazeOptions and packOptions objects defined in HW4 to access adjusted values
-    const calculatedPrice = ((rollBasePrice + glazeOptions[rollItem.glazing]) * packOptions[Number(rollItem.size)]).toFixed(2);
-    //get reference to totalprice outside template and set it to total
-    const total = document.querySelector(".cartTotalAmount");
-    console.log(total);
-    console.log(calculatedPrice);
-    total.innerText = `$ ${calculatedPrice}`;
+    const sumItemPrice = Number(((rollBasePrice + glazeOptions[rollItem.glazing]) * packOptions[Number(rollItem.size)])).toFixed(2);
+    templatePrice.innerText = `$ ${sumItemPrice}`;
 }
+
+calculatePrice();
+
+function calculatePrice(){
+    let totalPrice = 0;
+    const allPricesInCart = document.querySelectorAll(".templateItemPrice")
+    for (const price of allPricesInCart){
+        let rollPrice = Number((price.innerText).substring(1));//to remove the dollar sign 
+        totalPrice+=rollPrice;
+        console.log(rollPrice);
+    }
+    document.querySelector(".cartTotalAmount").innerText = `$ ${(totalPrice).toFixed(2)}`;
+}
+ 
 
 function deleteItem(rollItem){
     rollItem.element.remove();
-    containerElement.delete(rollItem);
+    cart.delete(rollItem);
+    calculatePrice();
 }
 
 
